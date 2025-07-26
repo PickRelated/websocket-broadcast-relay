f418f053fe35a454f486012d8b941027201fa0f9
Igor Tatarintsev <ietatarin@gmail.com>
2025-07-26 03:19:16 +0200

    Implement socket rooms

--- socket.js
+++ socket.js
@@ -1,24 +1,29 @@
 const { Server } = require('socket.io')
 const moment = require('moment')
 
-const socketHistory = {
-  events: [],
-}
+const socketHistory = {}
 
 setInterval(() => {
-  socketHistory.events = socketHistory.events.filter(({ time }) => moment().diff(time, 'seconds') <= 120)
+  for (const id in socketHistory) {
+    socketHistory[id] = socketHistory[id] || []
+    socketHistory[id] = socketHistory[id].filter(({ time }) => moment().diff(time, 'seconds') <= 120)
+  }
 }, 5000)
 
 const attach = (httpServer) => {
   const io = new Server(httpServer)
 
   io.on('connection', (socket) => {
-    console.info(`Connection from client ${socket.id}`)
+    const { id } = socket.handshake.query
+    console.info(`Connection from ${id}`)
+
+    socket.join(id)
 
     socket.onAny((event, payload) => {
-      console.info(`${socket.id} sent event ${event} with payload ${JSON.stringify(payload)}`)
-      socketHistory.events.push({ time: moment().toISOString(), socketId: socket.id, event, payload })
-      socket.broadcast.emit(event, payload)
+      console.info(`${id} [${event}]: ${JSON.stringify(payload)}`)
+      socketHistory[id] = socketHistory[id] || []
+      socketHistory[id].push({ time: moment().toISOString(), event, payload })
+      socket.to(id).emit(event, payload)
     })
   })
 }
